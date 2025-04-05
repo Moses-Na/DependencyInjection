b5e9fe6871e17d3708028729e3187072104e0ce4
Moses Natukwatsa <moses.na.43@gmail.com>
2025-04-05 16:15:55 +0300

    Commit #2
    In the the Old Version on line 1, 2 lines were involved in the change. In the the New Version on line 1, 12 lines were involved in the change

--- DependencyInjection/src/DataProcessor.java
+++ DependencyInjection/src/DataProcessor.java
@@ -1,2 +1,12 @@
 public class DataProcessor {
+    private final IDataService idataService;
+
+    public DataProcessor(IDataService iDataService){
+        this.idataService = iDataService;
+    }
+
+    public void process() {
+        String data = idataService.fetchData();
+        System.out.println("Processing: " + data);
+    }
 }

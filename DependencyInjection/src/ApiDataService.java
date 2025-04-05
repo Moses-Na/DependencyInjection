f4c00c6b165740a56168c55911765a3a7b60b4cc
Moses Natukwatsa <moses.na.43@gmail.com>
2025-04-05 15:55:12 +0300

    Commit #2
    In the the New Version on line 1, 12 lines were involved in the change

---
+++ DependencyInjection/src/ApiDataService.java
@@ -0,0 +1,12 @@
+public class ApiDataService implements IDataService{
+    private String apiKey;
+
+    public ApiDataService(String apiKey){
+        this.apiKey = apiKey;
+    }
+
+    @Override
+    public String fetchData(){
+        return "Data fetched from API using key: " + apiKey;
+    }
+}
